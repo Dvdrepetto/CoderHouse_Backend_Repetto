e4322b790631a40828acfa9c3e3a23da03f42b3e
Dvdrepetto <davidmrepetto@gmail.com>
2022-11-13 16:05:30 +0100

    desafio Servidor con express

---
+++ Clase5/Servidores_Con_Express.js/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const classContainer = require('./container/App')
+
+const app = express();
+const PORT = 8070
+
+//instanciando la clase
+const archivo = new classContainer('productos.JSON')
+
+
+app.get('/products',async (req, res)=>{
+    
+    const prods = await archivo.leer()
+
+    res.send({Productos: prods})
+})
+
+app.get('/productoRandom',async (req,res)=>{
+    const prods = await archivo.leer()
+    const random = parseInt(Math.random()* prods.length)
+
+    res.send({Productos: prods[random]})
+})
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+})
